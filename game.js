746447bd4d7ea203ae01f4f05fbd7cf96bf8d21d
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-01-13 14:24:47 -0700

    Working Health

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
 
+var health = 100;
+
+function slap(){
+	health = health - 1;
+	alert(health);
+}
+
+var update = document.getElementById('playerHealth')
+
+function updateHealth(){
+	update.innerText = health.toString();
+}
+
+updateHealth();
