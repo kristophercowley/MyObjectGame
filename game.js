cec30c9d0fbc4b0485f521225210107b014e44d5
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-01-14 15:39:36 -0700

    Refactored update function for cleaner functionality and less code

--- game.js
+++ game.js
@@ -1,9 +1,25 @@
+
+//Creates an array of objects with Item constructor
+var itemsObj = {
+	shield: new Item('Shield', 10, 'This is an awesome shield'),
+	redbull: new Item('RedBull',  20, 'YYeeeaaaEEAAEAh!!!'),
+	brass: new Item('Brass Knuckles', -10, 'Ouch!')
+}
+
 var player = {
 	name: "Arnold",
 	hits: 0,
-	health: 100
-}
+	health: 100,
+	items: [itemsObj.shield,itemsObj.RedBull],
+	}
 
+function addMods(){
+		var total = 0;
+		for(var i = 0; i < player.items.length; i ++){
+			console.log(player.items[i]);
+		}
+}
+addMods()
 var me = {
 	name: 'Me',
 	hits: 0,
@@ -34,11 +50,17 @@ function force() {
 	updateAll();
 }
 
+//Sets variables to placeholder IDs
+var update = document.getElementById('playerHealth');
+var playerName = document.getElementById('playerName');
+var displayHits = document.getElementById('hits');
+
 //Collection of update items to make an easier call
 function updateAll() {
-	updateHealth();
-	updateHits();
-	updateName();
+		update.innerText = player.health.toString();
+		playerName.innerText = player.name;
+		displayHits.innerText = player.hits.toString();
+		
 	if (player.health < 30) {
 		//health = 0;//not working
 		document.getElementById("player-panel").classList.add("panel-danger");
@@ -62,34 +84,4 @@ function Item(name, modifier, description) {
 	}
 }
 
-
-//Creates an array of objects with Item constructor
-var itemsObj = {
-	shield: new Item('Shield', player.health += 10, 'This is an awesome shield'),
-	redbull: new Item('RedBull', player.health += 20, 'YYeeeaaaEEAAEAh!!!'),
-	brass: new Item('Brass Knuckles', player.health -= 10, 'Ouch!')
-}
-
-var items = [itemsObj.shield,itemsObj.RedBull];
-
-//Sets variables to placeholder IDs
-var update = document.getElementById('playerHealth');
-var playerName = document.getElementById('playerName');
-var displayHits = document.getElementById('hits');
-
-//Functions to display point values to the page
-function updateHealth() {
-	update.innerText = player.health.toString();
-}
-
-function updateName() {
-	playerName.innerText = player.name;
-}
-
-function updateHits() {
-	displayHits.innerText = player.hits.toString();
-}
-
-updateHealth();
-updateName();
-updateHits();
+updateAll();
